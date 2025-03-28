e74707bb8f7214b40dc4a5b48c50f6d091ee6f2e
Levin010 <levinomwenga10@gmail.com>
2025-03-28 15:14:10 +0300

    Rename tenant-related functions to manager and update database queries accordingly

--- src/controllers/managerControllers.ts
+++ src/controllers/managerControllers.ts
@@ -3,39 +3,36 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma  = new PrismaClient();
 
-export const getTenant = async (req: Request, res: Response): Promise<void> => {
+export const getManager = async (req: Request, res: Response): Promise<void> => {
     try {
         const { cognitoId } = req.params;
-        const tenant = await prisma.tenant.findUnique({
-            where: { cognitoId },
-            include: {
-                favorites:true
-            }
+        const manager = await prisma.manager.findUnique({
+            where: { cognitoId }
         });
 
-        if (tenant) {
-            res.json(tenant)
+        if (manager) {
+            res.json(manager)
         } else {
-            res.status(404).json({ message: "Tenant not found"})
+            res.status(404).json({ message: "Manager not found"})
         }
     } catch (error: any ) {
-        res.status(500).json({ message: `Error retrieving tenant: ${error.message}`});
+        res.status(500).json({ message: `Error retrieving manager: ${error.message}`});
     }
 };
 
-export const createTenant = async (req: Request, res: Response): Promise<void> => {
+export const createManager = async (req: Request, res: Response): Promise<void> => {
     try {
         const { cognitoId, name, email, phoneNumber } = req.body;
 
-        const tenant = await prisma.tenant.create({
+        const manager = await prisma.manager.create({
             data: {
                 cognitoId, name, email, phoneNumber
             }
             }
         );
 
-        res.status(201).json(tenant);
+        res.status(201).json(manager);
     } catch (error: any ) {
-        res.status(500).json({ message: `Error creating tenant: ${error.message}`});
+        res.status(500).json({ message: `Error creating manager: ${error.message}`});
     }
 };
